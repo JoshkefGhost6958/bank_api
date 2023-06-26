77c59d963255e2f6cdddd9b907688a5052ec5cc0
Joshua <109339393+JoshkefGhost6958@users.noreply.github.com>
2023-06-26 10:35:47 +0300

    Update main.py cors config

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,Request
 from routers import transactions,user,authentication,registration
-
+from fastapi.security.cors import CORSMiddleware
 app = FastAPI()
 
 app.title = "Banking"
@@ -8,6 +8,13 @@ app.include_router(transactions.router)
 app.include_router(user.router)
 app.include_router(authentication.router)
 app.include_router(registration.router)
+origins=[
+  "http://localhost"
+]
+
+app.add_middleware(CORSMiddleware,
+                  allowed_origins=origins)
+
 
 @app.get("/")
 def home(request:Request):
